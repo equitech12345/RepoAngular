c4c2040716a696995fc25761bd24ab58646c896c
Juan Balladarez <juan.balladarez@gmail.com>
2024-09-06 14:46:01 +0100

    Se modifico el componente Not-funtion para que se vea mejor.

--- src/app/pages/not-funtion/not-funtion.component.ts
+++ src/app/pages/not-funtion/not-funtion.component.ts
@@ -5,12 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      not-funtion works!
-    </p>
+<div class="container">
+  <div class="alert alert-danger text-center mt-5" role="alert">
+    <h4 class="alert-heading">Not Functioning</h4>
+    <p>There is an issue with the current operation. Please try again later.</p>
+    <hr>
+    <div class="container-fluid" (click)="onReturnClicked()">
+        <button type="button" class="btn btn-warning w-20" >Volver</button>
+    </div>
+  </div>
+</div>
   `,
   styleUrl: './not-funtion.component.css'
 })
 export class NotFuntionComponent {
+  onReturnClicked(){
+    window.history.back()
+  }
 
 }
